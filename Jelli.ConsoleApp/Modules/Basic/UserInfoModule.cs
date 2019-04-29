a79bcdbe5b0f95d47f7789a632d9ac9086c24a0f
Keir Davis <keirjdavis@gmail.com>
2019-04-29 19:55:29 +0100

    fix(UserInfo): Added exception handling to UserInfo

--- Jelli.ConsoleApp/Modules/Basic/UserInfoModule.cs
+++ Jelli.ConsoleApp/Modules/Basic/UserInfoModule.cs
@@ -13,43 +13,50 @@ namespace Jelli.ConsoleApp.Modules.Basic
 		#region Methods
 		[Command("userinfo")]
 		[RequireContext(ContextType.Guild)]
-		public Task UserInfoAsync(IGuildUser user = null)
+		public async Task UserInfoAsync(IGuildUser user = null)
 		{
-			user = user ?? (IGuildUser)Context.User;
-
-			var description = $"{user.Status}";
-			if (user.Activity != null)
+			try
 			{
-				description += $", {user.Activity.Type} {user.Activity.Name}";
+				user = user ?? (IGuildUser)Context.User;
+
+				var description = $"{user.Status}";
+				if (user.Activity != null)
+				{
+					description += $", {user.Activity.Type} {user.Activity.Name}";
+				}
+
+				var title = $"{user}";
+				if (user.Nickname != null)
+				{
+					title += $"- {user.Nickname}";
+				}
+
+				var embed = new EmbedBuilder
+				{
+					Title = title,
+					// TODO Implement a better way of describing the current user's activity
+					Description = description,
+					ThumbnailUrl = user.GetAvatarUrl(),
+					Color = user.Guild.GetHighestRole(user)?.Color
+				};
+				// Joined Discord field
+				embed.AddField("Joined Discord", $"{user.CreatedAt.ToString("F")} ({user.DaysSinceCreated()} days ago)", inline: true);
+				// Joined server field
+				embed.AddField("Joined server", $"{user.JoinedAt?.ToString("F")} ({user.DaysSinceJoined()} days ago)", inline: true);
+				// Roles field
+				var usersRoles = user.Guild.GetUserRolesList(user);
+				if (usersRoles.Count > 0)
+				{
+					embed.AddField("Roles", $"{string.Join(", ", usersRoles)}")
+						.WithFooter(footer => footer.Text = $"User ID {user.Id}");
+				}
+
+				await ReplyAsync(embed: embed.Build());
 			}
-
-			var title = $"{user}";
-			if (user.Nickname != null)
+			catch (Exception)
 			{
-				title += $"- {user.Nickname}";
+				await ReplyAsync("Failed to get user info");
 			}
-
-			var embed = new EmbedBuilder
-			{
-				Title = title,
-				// TODO Implement a better way of describing the current user's activity
-				Description = description,
-				ThumbnailUrl = user.GetAvatarUrl(),
-				Color = user.Guild.GetHighestRole(user)?.Color
-			};
-			// Joined Discord field
-			embed.AddField("Joined Discord", $"{user.CreatedAt.ToString("F")} ({user.DaysSinceCreated()} days ago)", inline: true);
-			// Joined server field
-			embed.AddField("Joined server", $"{user.JoinedAt?.ToString("F")} ({user.DaysSinceJoined()} days ago)", inline: true);
-			// Roles field
-			var usersRoles = user.Guild.GetUserRolesList(user);
-			if (usersRoles.Count > 0)
-			{
-				embed.AddField("Roles", $"{string.Join(", ", usersRoles)}")
-					.WithFooter(footer => footer.Text = $"User ID {user.Id}");
-			}
-
-			return ReplyAsync(embed: embed.Build());
 		}
 		#endregion
 	}
